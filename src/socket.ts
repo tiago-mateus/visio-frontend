0a2c0480a600bc9449e1a39cda59fab16d475ad6
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-06-17 00:34:07 -0300

    ajuste

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io('https://visio-backend.vercel.app:3000');
+const socket = io('https://visio-backend-0355e9bc0d18.herokuapp.com');
 
 export default socket;
