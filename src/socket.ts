0773dcb9c85d9a8d5262102b00d52e6327af4d27
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-06-17 00:06:15 -0300

    teste

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io('https://visio-backend.vercel.app', {
-    transports: ['websocket']
-});
+const socket = io('https://visio-backend.vercel.app:3000');
 
 export default socket;
